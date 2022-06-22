769f182df79b62123dc88f4883b09f8592e1e5ec
Titus <titus.kamunya@student.moringaschool.com>
2022-06-22 14:49:26 +0300

    this commit works on data validation

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 
 
 
-const name = document.getElementById('name')
-const password = document.getElementById('message')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+// const name = document.getElementById('name')
+// const password = document.getElementById('message')
+// const form = document.getElementById('form')
+// const errorElement = document.getElementById('error')
 
-form.addEventListener('submit', (e) => {
-  let messages = []
-  if (name.value === '' || name.value == null) {
-    messages.push('Name is required')
-  }
+// form.addEventListener('submit', (e) => {
+//   let messages = []
+//   if (name.value === '' || name.value == null) {
+//     messages.push('Name is required')
+//   }
 
-  if (message.value.length <= 6) {
-    messages.push('message must be longer than 6 characters')
-  }
+//   if (message.value.length <= 6) {
+//     messages.push('message must be longer than 6 characters')
+//   }
 
-  if (password.value.length >= 20) {
-    messages.push('message must be less than 200 characters')
-  }
+//   if (password.value.length >= 20) {
+//     messages.push('message must be less than 200 characters')
+//   }
 
-  if (password.value === 'password') {
-    messages.push('Password cannot be password')
-  }
+//   if (password.value === 'password') {
+//     messages.push('Password cannot be password')
+//   }
 
-  if (messages.length > 0) {
-    e.preventDefault()
-    errorElement.innerText = messages.join(', ')
-  }
-})
+//   if (messages.length > 0) {
+//     e.preventDefault()
+//     errorElement.innerText = messages.join(', ')
+//   }
+// })
+
+const f = document.getElementById('form');
+      const q = document.getElementById('query');
+      const google = 'https://www.google.com/search?q=site%3A+';
+      const site = 'https://www.blockchaingrants.org/';
+
+      function submitted(event) {
+        event.preventDefault();
+        const url = google + site + '+' + q.value;
+        const win = window.open(url, '_blank');
+        win.focus();
+      }
+
+      f.addEventListener('submit', submitted);
