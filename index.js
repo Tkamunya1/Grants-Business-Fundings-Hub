98f0b5e7686ca6f350e7d51638744dcbe03da8a4
Titus <titus.kamunya@student.moringaschool.com>
2022-06-30 17:01:36 +0300

    Done

--- index.js
+++ index.js
@@ -45,9 +45,10 @@ fetch(url)
 			html += `
 			 <div class = "col.md-9">
 			 <div class="card">
+                      
                       <h4 class="title"><span>Title</span>: ${value.title}</h4>
-                      <h4 id="author"><span>Author</span>: ${value.author}</h4>
                       <img class="card-img-top" src="${value.imgUrl}" alt="Card image" style="width:10%">
+                      <h4 id="author"><span>Author</span>: ${value.author}</h4>
 
                  
              </div>
